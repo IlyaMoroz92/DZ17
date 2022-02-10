295489fdd85ae2fef823676cebd4ead236aea200
Ilya Moroz <mermermer@inbox.ru>
2022-02-10 18:42:44 +0300

    'add'

--- js/script.js
+++ js/script.js
@@ -1,19 +1,33 @@
-const button = document.querySelector(".button");
+  const button = document.querySelector(".button");
+  const stepBtn = document.querySelector(".button-step");
   const input = document.querySelector(".input");
   const result = document.querySelector(".result");
+  const stepResult = document.querySelector(".result-step");
 
   let factorial = 1;
+  let stepFactorial = 1;
+  let what = stepGenerator();
 
-  function* generator() {
+  function generator() {
     let num = input.value;
     for (i = 1; i <= num; i++) {
       factorial = factorial * i;
-      console.log(i);
       result.innerHTML = factorial;
+      button.style.display = "none";
     }
   }
 
-  button.addEventListener("click", function () {
-    let gen = generator(input.value);
-    gen.next();
+  function* stepGenerator(i = 1) {
+    while (i <= input.value) {
+      stepFactorial = stepFactorial * i;
+      stepResult.innerHTML = stepFactorial;
+      yield i;
+      i++;
+    }
+  }
+
+  button.addEventListener("click", generator);
+
+  stepBtn.addEventListener("click", function () {
+    what.next();
   });
